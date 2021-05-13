8574344370ecd3fd65a44f311f6ba88f6965d470
Jakub Posłuszny <jjposluszny@protonmail.com>
2021-05-13 18:27:05 +0200

    Basic horizontal movement

--- Assets/Scripts/Tank.cs
+++ Assets/Scripts/Tank.cs
@@ -6,6 +6,7 @@ public class Tank : MonoBehaviour
 {
     [SerializeField] public Rigidbody2D avatar;
     [SerializeField] private float enginePower = 1.0f;
+    private float _horizontalVelocityRatio = .0f;
     void Start()
     {
         
@@ -13,6 +14,10 @@ public class Tank : MonoBehaviour
 
     public void Move(float axisRatio) {
         //avatar.AddForce(transform.right * axisRatio * enginePower, mode: ForceMode2D.Force);
-        avatar.AddRelativeForce(transform.right * axisRatio * enginePower, mode: ForceMode2D.Force);
+        _horizontalVelocityRatio = axisRatio;
+    }
+
+    private void FixedUpdate() {
+        avatar.velocity += (Vector2)this.transform.right * _horizontalVelocityRatio * enginePower/1000.0f;
     }
 }
